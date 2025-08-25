721903f910c8bf1083908946313ffb5a54bdff44
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-25 13:15:05 +0700

    Update App.js

--- cloud-android/backend/frontend/src/App.js
+++ cloud-android/backend/frontend/src/App.js
@@ -1,1 +1,15 @@
+import React, { useState } from 'react';
+import Login from './Login';
+import AndroidScreen from './AndroidScreen';
 
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <div className="App">
+      {loggedIn ? <AndroidScreen /> : <Login onLogin={setLoggedIn} />}
+    </div>
+  );
+}
+
+export default App;
