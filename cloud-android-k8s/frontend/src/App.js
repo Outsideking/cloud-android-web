aaf20066a00350415a061a24a7c3ac316f77c691
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-25 14:09:35 +0700

    Update App.js

--- cloud-android-k8s/frontend/src/App.js
+++ cloud-android-k8s/frontend/src/App.js
@@ -1,1 +1,11 @@
+import React, { useState } from 'react';
+import Login from './Login';
+import AndroidScreen from './AndroidScreen';
+
+function App(){
+  const [podName,setPodName] = useState(null);
+  return podName ? <AndroidScreen podName={podName}/> : <Login onLogin={setPodName}/>;
+}
+
+export default App;
 
