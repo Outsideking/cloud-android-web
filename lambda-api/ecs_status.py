1db7943e6b64555f33d0c6f53b28d72733824f34
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-26 01:54:24 +0700

    Update ecs_status.py

--- lambda-api/ecs_status.py
+++ lambda-api/ecs_status.py
@@ -1,12 +1,21 @@
-FROM dorowu/ubuntu-desktop-lxde-vnc:focal
+import boto3
+import json
 
-RUN apt-get update && apt-get install -y \
-    android-tools-adb \
-    android-sdk \
-    && rm -rf /var/lib/apt/lists/*
+def lambda_handler(event, context):
+    ecs = boto3.client("ecs", region_name="ap-southeast-2")
+    elbv2 = boto3.client("elbv2", region_name="ap-southeast-2")
 
-COPY start.sh /start.sh
-RUN chmod +x /start.sh
+    cluster_name = "cloud-android-cluster"
+    service_name = "cloud-android-web"
 
-EXPOSE 6080
-CMD ["/start.sh"]
+    service = ecs.describe_services(cluster=cluster_name, services=[service_name])["services"][0]
+    lb_name = service["loadBalancers"][0]["loadBalancerName"]
+
+    lb = elbv2.describe_load_balancers(Names=[lb_name])["LoadBalancers"][0]
+    dns = f"http://{lb['DNSName']}:6080"
+
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps({"url": dns})
+    }
