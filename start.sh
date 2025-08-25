f7ab69c5edc672655eb5f151a8a8fb9c8540ab58
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-26 01:50:23 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
-set -e
-
-export DISPLAY=:0
-
-# Start X virtual framebuffer
-Xvfb :0 -screen 0 1280x720x16 &
-
-# Start window manager
-fluxbox &
-
-# Start VNC server
-x11vnc -forever -nopw -display :0 -rfbport 5900 &
-
-# Start noVNC web server
-websockify --web=/usr/share/novnc/ 6080 localhost:5900 &
-
-# Start Android emulator
-emulator -avd test -noaudio -no-boot-anim -accel on -gpu swiftshader_indirect &
-
-# Keep container running
-tail -f /dev/null
+echo "Starting Cloud Android Web..."
+/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
