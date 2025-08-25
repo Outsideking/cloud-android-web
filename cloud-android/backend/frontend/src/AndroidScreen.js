4bca8fd6b221eb534814ad2e26a07c5aba684516
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-25 13:14:41 +0700

    Update AndroidScreen.js

--- cloud-android/backend/frontend/src/AndroidScreen.js
+++ cloud-android/backend/frontend/src/AndroidScreen.js
@@ -1,1 +1,16 @@
+import React, { useEffect, useRef } from 'react';
+import RFB from 'novnc/core/rfb.js';
 
+export default function AndroidScreen() {
+  const vncRef = useRef(null);
+
+  useEffect(() => {
+    const rfb = new RFB(vncRef.current, 'ws://localhost:6080');
+    rfb.viewOnly = false;
+    rfb.scaleViewport = true;
+
+    return () => rfb.disconnect();
+  }, []);
+
+  return <div ref={vncRef} style={{ width: '1280px', height: '720px', border: '2px solid black' }}></div>;
+}
